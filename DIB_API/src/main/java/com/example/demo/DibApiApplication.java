6fca0957375893203cd6ec9e8365da0d1ba67603
Milos Cirkovic <cirkovicmilos99@gmail.com>
2021-08-21 14:01:43 +0200

    Delete comments

--- DIB_API/src/main/java/com/example/demo/DibApiApplication.java
+++ DIB_API/src/main/java/com/example/demo/DibApiApplication.java
@@ -1,15 +1,8 @@
 package com.example.demo;
 
-import java.util.Collections;
-
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 
 @SpringBootApplication
@@ -18,20 +11,5 @@ public class DibApiApplication {
 		SpringApplication.run(DibApiApplication.class, args);
 	}
 	
-//	  // Fix the CORS errors
-//    @Bean
-//    public FilterRegistrationBean simpleCorsFilter() {  
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
-//        CorsConfiguration config = new CorsConfiguration();  
-//        config.setAllowCredentials(true); 
-//        // *** URL below needs to match the Vue client URL and port ***
-//        config.setAllowedOrigins(Collections.singletonList("http://localhost:8090")); 
-//        config.setAllowedMethods(Collections.singletonList("*"));  
-//        config.setAllowedHeaders(Collections.singletonList("*"));  
-//        source.registerCorsConfiguration("/**", config); 
-//        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter());
-//        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
-//        return bean;  
-//    }   
 
 }
